0c78a04d86108eafdd38f4d10084c310710f3ed7
hongci9999 <hongci9999@gmail.com>
2025-01-26 02:55:03 +0900

    exhibition 추가가

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Trade from './pages/Trade/Trade';
+import Exhibition from "./pages/Exhibition/Exhibition";
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/trade" element={<Trade />} />
+          <Route path="/exhibition" element={<Exhibition />} />
         </Routes>
       </div>
     </Router>
---
+++ src/pages/Exhibition/Exhibition.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Exhibition.module.css';
+
+function Exhibition() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Welcome to the Home Page</h1>
+      <p>This is the main page of the application.</p>
+    </div>
+  );
+}
+
+export default Exhibition;
