068c29f2a0aee87ee21f942e9d80ea8a2b0bf4f2
Yoon Ji-Yoon <dbswldbs0723@gmail.com>
2025-01-28 12:14:48 +0900

    aaaa

--- src/pages/Exhibition/Exhibition.jsx
+++ src/pages/Exhibition/Exhibition.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import styles from './Exhibition.module.css';
+import React from "react";
+import styles from "./Exhibition.module.css";
 
 function Exhibition() {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Welcome to the Home Page</h1>
       <p>This is the main page of the application.</p>
+      <div></div>
     </div>
   );
 }
